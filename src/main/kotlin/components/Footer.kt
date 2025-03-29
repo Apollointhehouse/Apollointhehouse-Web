3754a757c1590db5acc7376fec81944809eeac5b
Apollointhehouse <99405787+Apollointhehouse@users.noreply.github.com>
2025-03-29 17:01:34 +1300

    Added about me section to homepage and improved footer!

--- src/main/kotlin/components/Footer.kt
+++ src/main/kotlin/components/Footer.kt
@@ -7,21 +7,38 @@ private val socials = listOf(
         name = "Github",
         url = "https://github.com/apollointhehouse",
         icon = "/images/github.png",
-    )
+    ),
+    Social(
+        name = "LinkedIn",
+        url = "https://linkedin.com/in/apollo-cameron-boot-8061a12b8",
+        icon = "/images/linkedin.png",
+    ),
+    Social(
+        name = "Bluesky",
+        url = "https://bsky.app/profile/apollointhehouse.bsky.social",
+        icon = "/images/bluesky.png",
+    ),
+
 )
 
 fun FlowContent.footer() = footer {
-    div("container") {
-        div("social") {
-            p { +"Contact me:" }
+    nav("container") {
+        ul {
+            li {
+                a(href = "https://github.com/Apollointhehouse/Apollointhehouse-Web") { small { +"Source" } }
+            }
+        }
 
+        ul {
             for (social in socials) {
-                a {
-                    href = social.url
-                    target = "_blank"
-                    img(classes = "dark-mode") {
-                        src = social.icon
-                        alt = social.name
+                li("social") {
+                    a("contrast") {
+                        href = social.url
+                        target = "_blank"
+                        img(classes = "dark-mode") {
+                            src = social.icon
+                            alt = social.name
+                        }
                     }
                 }
             }
--- src/main/kotlin/pages/Index.kt
+++ src/main/kotlin/pages/Index.kt
@@ -15,8 +15,21 @@ fun HTML.index() = base("Home") {
             }
         }
     }
-    main(classes = "container") {
-        h1 { +"Work in Progress!" }
+    main("container") {
+        section("about-me") {
+            h3 { +"About Me" }
+            p { +"I am a high school student from New Zealand, planning to attend the University of Auckland." }
+            p { +"I am particularly interested in backend development, especially using Kotlin." }
+            h3 { +"Skills" }
+            ul {
+                li { +"Proficient in Kotlin, Java, Python, HTML, CSS, and JavaScript." }
+                li { +"Backend: SQL, Ktor, Flask" }
+            }
+            h3 { +"Experience" }
+            p { +"Currently, I have no professional experience, but I have participated in the Terrible Ideas Hackathon, where I worked effectively in a team. Additionally, I have collaborated on several shared GitHub projects with individuals of varying experience levels." }
+            h3 { +"Education" }
+            p { +"Senior High School Student, planning to attend the University of Auckland." }
+        }
     }
     footer()
 }
