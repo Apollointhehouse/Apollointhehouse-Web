ae93f2a8edb5648e05223612b344d620009a1f94
Apollointhehouse <apollocameronboot@gmail.com>
2025-05-18 22:32:37 +1200

    Moved img from Skills to Metrics

--- src/main/kotlin/pages/Index.kt
+++ src/main/kotlin/pages/Index.kt
@@ -42,10 +42,10 @@ fun HTML.index() = base("Home") {
                 }
             }
 
-            img {
-                src = "https://github-readme-stats-one-orcin.vercel.app/api/top-langs?username=apollointhehouse&show_icons=true&bg_color=24273a&text_color=cad3f5&icon_color=c6a0f6&title_color=8bd5ca&hide_border=false&layout=compact&locale=en"
-                alt = "Apollo's Most Used Languages"
-            }
+//            img {
+//                src = "https://github-readme-stats-one-orcin.vercel.app/api/top-langs?username=apollointhehouse&show_icons=true&bg_color=24273a&text_color=cad3f5&icon_color=c6a0f6&title_color=8bd5ca&hide_border=false&layout=compact&locale=en"
+//                alt = "Apollo's Most Used Languages"
+//            }
         }
 
         article("experience") {
@@ -58,12 +58,19 @@ fun HTML.index() = base("Home") {
             p { +"Senior High School Student, planning to attend the University of Auckland." }
         }
 
-        article("stats") {
-            h3 { +"Stats" }
+        article("metrics") {
+            h3 { +"Metrics" }
 
             img {
-                src = "https://github-readme-stats-one-orcin.vercel.app/api?username=apollointhehouse&show_icons=true&bg_color=24273a&text_color=cad3f5&icon_color=c6a0f6&title_color=8bd5ca&hide_border=false&locale=en"
+                src =
+                    "https://github-readme-stats-one-orcin.vercel.app/api?username=apollointhehouse&show_icons=true&bg_color=24273a&text_color=cad3f5&icon_color=c6a0f6&title_color=8bd5ca&hide_border=false&locale=en"
                 alt = "Apollo's Github Stats"
+            }
+
+            img {
+                src =
+                    "https://github-readme-stats-one-orcin.vercel.app/api/top-langs?username=apollointhehouse&show_icons=true&bg_color=24273a&text_color=cad3f5&icon_color=c6a0f6&title_color=8bd5ca&hide_border=false&layout=compact&locale=en"
+                alt = "Apollo's Most Used Languages"
             }
         }
     }
