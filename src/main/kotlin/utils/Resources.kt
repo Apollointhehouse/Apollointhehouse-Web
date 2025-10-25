d95191035d89f4dfd6ecd96a3bd5844b8ebc3b46
Apollointhehouse <apollocameronboot@gmail.com>
2025-10-25 15:13:13 +1300

    fixed race condition

--- src/main/kotlin/utils/Resources.kt
+++ src/main/kotlin/utils/Resources.kt
@@ -8,8 +8,6 @@ import java.io.File
 import java.net.URI
 
 object Resources {
-    private val scope = CoroutineScope(Dispatchers.IO)
-
     val htmx = download("htmx.min.js", "https://cdn.jsdelivr.net/npm/htmx.org@2.0.7/dist/htmx.min.js")
     val picoCSS = download("pico.min.css", "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css")
 
@@ -23,12 +21,10 @@ object Resources {
     }
 
     fun imgDownload(name: String, url: String, ext: String): String {
-        scope.launch {
-            val dir = File("${Config.base}/assets/images/$name.$ext")
-            dir.createNewFile()
+        val dir = File("${Config.base}/assets/images/$name.$ext")
+        dir.createNewFile()
 
-            dir.writeBytes(URI(url).toURL().readBytes())
-        }
+        dir.writeBytes(URI(url).toURL().readBytes())
 
         return "/assets/images/$name.$ext"
     }
