2a76617b562cbe607c040358cda9cf56c67cc2ce
Apollointhehouse <me@apollointhehouse.dev>
2026-05-04 23:09:28 +1200

    Remove linkedin redirect page!

--- src/main/kotlin/me/apollointhehouse/Main.kt
+++ src/main/kotlin/me/apollointhehouse/Main.kt
@@ -18,7 +18,7 @@ fun main() {
         "/CV" to { CV() },
         "/test" to { redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ") },
         "/github" to { redirect("https://github.com/apollointhehouse") },
-        "/linkedin" to { redirect("https://linkedin.com/in/apollo-cameron-boot-8061a12b8") }
+//        "/linkedin" to { redirect("https://linkedin.com/in/apollo-cameron-boot-8061a12b8") }
     )
 
     println("Done!")
